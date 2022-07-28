9b37891841e7e78c48cca578ed7ffc6f34b9618f
AMTATO <ilhampradipta12@gmail.com>
2022-07-28 11:26:28 +0700

    Mengenal component

---
+++ src/Navbar.js
@@ -0,0 +1,32 @@
+// Class Components
+// import React, { Component } from 'react'
+
+// export default class Navbar extends Component {
+//   render() {
+//     return (
+//       <h3>Navbar</h3>
+//     )
+//   }
+// }
+
+// Function Components
+// import React from 'react'
+
+// export default function Navbar() {
+//   return (
+//     <h3>Navbar</h3>
+//   )
+// }
+
+// Arrow Function
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h3>Navbar</h3>
+    </div>
+  )
+}
+
+export default Navbar
