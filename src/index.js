4f4fa1ad51d3365c98bba52f660aa25e90cae18a
AMTATO <ilhampradipta12@gmail.com>
2022-08-04 13:25:34 +0700

    Props dan State

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 // import App from './App';
 import Header from './Header';
 import Navbar from './Navbar';
+import StateProps from './StateProps';
 import Variable from './Variable';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar />
+    {/* <Navbar />
     <Header />
-    {/* <App /> */}
-    <Variable />
+    <App />
+    <Variable /> */}
+    <StateProps />
   </React.StrictMode>
 );
